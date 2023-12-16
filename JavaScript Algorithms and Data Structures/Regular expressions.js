b16519303ca979c43177298b57c992bdd886e251
urirmz <ing.urirmz@gmail.com>
2023-12-15 18:22:14 -0600

    Update Regular expressions.js

--- JavaScript Algorithms and Data Structures/Regular expressions.js
+++ JavaScript Algorithms and Data Structures/Regular expressions.js
@@ -68,3 +68,54 @@
 // let jennyStr = "Jenny8675309";
 // let myRegex = /[a-z0-9]/ig;
 // jennyStr.match(myRegex);
+
+// Match characters not specified
+// You could also create a set of characters that you do not want to match. These types of character sets are called negated character sets.
+// To create a negated character set, you place a caret character (^) after the opening bracket and before the characters you do not want to match.
+// For example, /[^aeiou]/gi matches all characters that are not a vowel. Note that characters like ., !, [, @, / and white space are matched - the negated vowel character set only excludes the vowel characters.z
+
+// The + expression
+// Sometimes, you need to match a character (or group of characters) that appears one or more times in a row. This means it occurs at least once, and may be repeated.
+// You can use the + character to check if that is the case. Remember, the character or pattern has to be present consecutively. That is, the character has to repeat one after the other.
+// For example, /a+/g would find one match in abc and return ["a"]. Because of the +, it would also find a single match in aabc and return ["aa"].
+// If it were instead checking the string abab, it would find two matches and return ["a", "a"] because the a characters are not in a row - there is a b between them. Finally, since there is no a in the string bcd, it wouldn't find a match.
+
+// The * expression
+// An option that matches characters that occur zero or more times.
+// The character to do this is the asterisk or star: *.
+// let soccerWord = "gooooooooal!";
+// let gPhrase = "gut feeling";
+// let oPhrase = "over the moon";
+// let goRegex = /go*/;
+// soccerWord.match(goRegex);
+// gPhrase.match(goRegex);
+// oPhrase.match(goRegex);
+// In order, the three match calls would return the values ["goooooooo"], ["g"], and null.
+
+// Greedy and lazy matches 
+// a greedy match finds the longest possible part of a string that fits the regex pattern and returns it as a match. The alternative is called a lazy match, which finds the smallest possible part of the string that satisfies the regex pattern.
+// You can apply the regex /t[a-z]*i/ to the string "titanic". This regex is basically a pattern that starts with t, ends with i, and has some letters in between.
+// Regular expressions are by default greedy, so the match would return ["titani"]. It finds the largest sub-string possible to fit the pattern.
+// However, you can use the ? character to change it to lazy matching. "titanic" matched against the adjusted regex of /t[a-z]*?i/ returns ["ti"].
+// Note: Parsing HTML with regular expressions should be avoided, but pattern matching an HTML string with regular expressions is completely fine.
+
+// The caret character (^)
+// The caret character (^) inside a character creates a negated character set in the form [^thingsThatWillNotBeMatched]. Outside of a character set, the caret is used to search for patterns at the beginning of strings.
+// let firstString = "Ricky is first and can be found.";
+// let firstRegex = /^Ricky/;
+// firstRegex.test(firstString);
+// let notFirst = "You can't find Ricky now.";
+// firstRegex.test(notFirst);
+// The first test call would return true, while the second would return false.
+
+// Match ending string patterns
+// You can search the end of strings using the dollar sign character $ at the end of the regex.
+// let theEnding = "This is a never ending story";
+// let storyRegex = /story$/;
+// storyRegex.test(theEnding);
+// let noEnding = "Sometimes a story will have to end";
+// storyRegex.test(noEnding);
+// The first test call would return true, while the second would return false.
+
+// Match all characters and numbers 
+// The closest character class in JavaScript to match the alphabet is \w. This shortcut is equal to [A-Za-z0-9_]. This character class matches upper and lowercase letters plus numbers. Note, this character class also includes the underscore character (_).
