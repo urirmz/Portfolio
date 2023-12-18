72be60984fe2a12dd5ff06ae800a96f4d50121fc
urirmz <ing.urirmz@gmail.com>
2023-12-17 20:01:52 -0600

    Started debugging

---
+++ JavaScript Algorithms and Data Structures/Debugging.js
@@ -0,0 +1,11 @@
+// Use the JavaScript Console to Check the Value of a Variable
+// Both Chrome and Firefox have excellent JavaScript consoles, also known as DevTools, for debugging your JavaScript.
+// You can find Developer tools in your Chrome's menu or Web Console in Firefox's menu.
+// The console.log() method, which "prints" the output of what's within its parentheses to the console, will likely be the most helpful debugging tool. Placing it at strategic points in your code can show you the intermediate values of variables. It's good practice to have an idea of what the output should be before looking at what it is. Having check points to see the status of your calculations throughout your code will help narrow down where the problem is.
+// Here's an example to print the string Hello world! to the console:
+// console.log('Hello world!');
+
+// The Javascript console
+// There are many methods to use with console to output messages. log, warn, and clear to name a few. The freeCodeCamp console will only output log messages, while the browser console will output all messages. When you make changes to your code, it will automatically run and show the logs. The freeCodeCamp console is then cleared each time your code runs.
+// First, open your browser console so you can see the logs. To do that, you can right-click the freeCodeCamp navigation bar at the top and click inspect on most browsers. Then find the console tab in the window that opens.
+// After that, use console.log to log the output variable. View the two consoles to see the log. Finally, use console.clear after your log to clear the browser console. 
