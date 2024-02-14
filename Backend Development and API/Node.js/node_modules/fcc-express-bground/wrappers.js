daa3a1125ae9d5fc2b8b263ca9bad182c6096269
urirmz <ing.urirmz@gmail.com>
2024-02-13 21:28:09 -0600

    fix: unecessary files

---
+++ Backend Development and API/Node.js/node_modules/fcc-express-bground/wrappers.js
@@ -0,0 +1,55 @@
+/******************************************************
+ * console.log wrapper
+ * to test 1) Hello World, 7) Logger middleware
+ * ***************************************************/
+
+const globals = require('./globals');
+const ip = require("ip");
+
+// Store a reference to the original console.log()
+const log = console.log;
+
+// Wrap console.log() to run a check everytime before it's used
+console.log = function () {
+  // Convert arguments into actual array.
+  const args = Array.prototype.slice.call(arguments);
+
+  // Check if this message solves any of the challenges that make use of console.log()
+  testLogMessage(args);
+
+  // Execute the original console.log() provided args
+  log.apply(null, args);
+};
+
+// Verify if a log message solves the expected log for certain challenges
+function testLogMessage(args) {
+  let msg = '';
+  try {
+    // build the log string
+    msg = args
+      .map(function (arg) {
+        return arg && arg.toString();
+      })
+      .join(' ');
+  } catch (e) {
+    log('log error - invalid arguments passed to console.log', e);
+  }
+
+  // Check if it solves the hello-console challenge.
+  if (msg.toLowerCase().match(/^hello,?\sworld!?$/)) {
+    globals.userPassedConsoleChallenge = true;
+  }
+
+  // Check if it solves the simple-middleware-logger challenge.
+  const simpleLogSplit = msg.split("-");
+  const ipAddress = simpleLogSplit[1];
+  if (
+    msg.match(/(GET|POST|PUT|DELETE|CONNECT|HEAD|OPTIONS|TRACE)\s\/.*\s\-/)
+  ) {
+    if (ip.isV4Format(ipAddress.trim()) || ip.isV6Format(ipAddress.trim())) {
+      globals.userPassedLoggerChallenge = true;
+    }
+  }
+}
+
+module.exports = log;
